4a2b9b274d4a3eecc8d15d2aac507615a8186c4d
Jingjing Shi <js884@duke.edu>
2021-03-08 00:56:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 import click
 
-@click.command()
-def hello():
-    click.echo('Hello World!')
 
+@click.command()
+@click.option('--type')
+@click.option('--temp',type=float)
+def conversion(type,temp):
+    
+    '''
+    A conversion of celsius to fahrenheit.
+    formula (0°C × 9/5) + 32 = 32°F
+    '''
+    
+    if type == 'F':
+        result = (temp-32)/(9/5)
+    elif type == 'C':
+        result = (temp*9/5)+32
+    else:
+        result = 'Please specify the type of temperature'
+    
+    click.echo(result)
 if __name__ == '__main__':
-    hello()
+    #pylint: disable=no-value-for-parameter
+    conversion()
